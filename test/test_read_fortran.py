e82fb7c9d2b792b68660339d657dd46cf9bb5c63
Julian Lenz <j.j.lenz@swansea.ac.uk>
2023-11-02 15:03:21 +0000

    Some non-trivial tests

--- test/test_read_fortran.py
+++ test/test_read_fortran.py
@@ -3,6 +3,7 @@
 from io import StringIO
 from typing import TextIO
 
+import numpy as np
 import pytest
 
 from glue_analysis.readers.read_fortran import _read_correlators_fortran
@@ -16,6 +17,27 @@ def filename() -> str:
 @pytest.fixture()
 def trivial_file() -> StringIO:
     return StringIO("column-name")
+
+
+@pytest.fixture()
+def columns() -> list[str]:
+    return ["this", "is-so-very-much", "a-random-string"]
+
+
+@pytest.fixture()
+def data(columns: list[str]) -> np.array:
+    return np.random.randint(0, 10, (10, len(columns)))
+
+
+@pytest.fixture()
+def corr_file(columns: list[str], data: np.array) -> StringIO:
+    memory_file = StringIO()
+    np.savetxt(memory_file, data)
+    memory_file.seek(0)
+    return StringIO(" ".join(columns) + "\n" + memory_file.read())
+
+
+### Trivial behavior
 
 
 def test_read_correlators_fortran_records_filename(
@@ -51,3 +73,28 @@ def test_read_correlators_fortran_freezes_the_ensemble(
 ) -> None:
     answer = _read_correlators_fortran(trivial_file, filename)
     assert answer._frozen
+
+
+### Actually functional behavior
+
+
+def test_read_correlators_fortran_passes_on_metadata(
+    trivial_file: TextIO, filename: str
+) -> None:
+    metadata = {"some": "thing"}
+    answer = _read_correlators_fortran(trivial_file, filename, metadata=metadata)
+    assert answer.metadata == metadata
+
+
+def test_read_correlators_fortran_preserves_column_names(
+    corr_file: TextIO, filename: str, columns: list[str]
+) -> None:
+    answer = _read_correlators_fortran(corr_file, filename)
+    assert set(answer.correlators.columns) == set(columns + ["channel"])
+
+
+def test_read_correlators_fortran_preserves_data(
+    corr_file: TextIO, filename: str, data: list[str]
+) -> None:
+    answer = _read_correlators_fortran(corr_file, filename)
+    assert (answer.correlators.drop("channel", axis=1).values == data).all()
