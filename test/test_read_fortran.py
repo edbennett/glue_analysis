cc67b1ffd8884c70e52c92441d1f6c3a06747972
Julian Lenz <j.j.lenz@swansea.ac.uk>
2023-11-02 14:26:16 +0000

    Add trivial tests

--- test/test_read_fortran.py
+++ test/test_read_fortran.py
@@ -23,3 +23,31 @@ def test_read_correlators_fortran_records_filename(
 ) -> None:
     answer = _read_correlators_fortran(trivial_file, filename)
     assert answer.filename == filename
+
+
+def test_read_correlators_fortran_creates_channel_column(
+    trivial_file: TextIO, filename: str
+) -> None:
+    answer = _read_correlators_fortran(trivial_file, filename)
+    assert "channel" in answer.correlators.columns
+
+
+def test_read_correlators_fortran_does_not_create_vev_if_not_given(
+    trivial_file: TextIO, filename: str
+) -> None:
+    answer = _read_correlators_fortran(trivial_file, filename)
+    assert "vevs" not in dir(answer)
+
+
+def test_read_correlators_fortran_sets_not_given_metadata_to_empty_dict(
+    trivial_file: TextIO, filename: str
+) -> None:
+    answer = _read_correlators_fortran(trivial_file, filename)
+    assert answer.metadata == {}
+
+
+def test_read_correlators_fortran_freezes_the_ensemble(
+    trivial_file: TextIO, filename: str
+) -> None:
+    answer = _read_correlators_fortran(trivial_file, filename)
+    assert answer._frozen
