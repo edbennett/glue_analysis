7cd8202d598c9cbe11f2e4708547a5885721248c
Julian Lenz <j.j.lenz@swansea.ac.uk>
2023-11-02 14:14:44 +0000

    Refactor test_read_fortran

--- test/test_read_fortran.py
+++ test/test_read_fortran.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
 from io import StringIO
+from typing import TextIO
+
+import pytest
 
 from glue_analysis.readers.read_fortran import _read_correlators_fortran
 
 
-def test_read_correlators_fortran_records_filename() -> None:
-    filename = "testname.txt"
-    corr_file = StringIO("column-name")
-    answer = _read_correlators_fortran(corr_file, filename)
+@pytest.fixture()
+def filename() -> str:
+    return "testname.txt"
+
+
+@pytest.fixture()
+def trivial_file() -> StringIO:
+    return StringIO("column-name")
+
+
+def test_read_correlators_fortran_records_filename(
+    trivial_file: TextIO, filename: str
+) -> None:
+    answer = _read_correlators_fortran(trivial_file, filename)
     assert answer.filename == filename
