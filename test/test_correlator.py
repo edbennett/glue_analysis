dae91e151176fa970f5dd1c858e723bd30438ebe
Julian Lenz <j.j.lenz@swansea.ac.uk>
2023-11-06 16:57:18 +0000

    Test NT

--- test/test_correlator.py
+++ test/test_correlator.py
@@ -3,10 +3,39 @@ import pytest
 
 from glue_analysis.correlator import CorrelatorData, CorrelatorEnsemble, VEVData
 
+LENGTH_BIN_INDEX = 1
+LENGTH_TIME = 2
+LENGTH_OP_INDEX = 3
+CORRELATOR_DATA_LENGTH = LENGTH_TIME * LENGTH_BIN_INDEX * LENGTH_OP_INDEX**2
+
 
 @pytest.fixture()
 def corr_data() -> CorrelatorData:
-    return pd.DataFrame()
+    return (
+        pd.MultiIndex.from_product(
+            [
+                range(1, LENGTH_BIN_INDEX + 1),
+                range(1, LENGTH_TIME + 1),
+                range(1, LENGTH_OP_INDEX + 1),
+                range(1, LENGTH_OP_INDEX + 1),
+            ],
+            names=["Bin_index", "Time", "Op_index1", "Op_index2"],
+        )
+        .to_frame()
+        .assign(glue_bins=range(CORRELATOR_DATA_LENGTH))
+    )
+
+
+@pytest.fixture()
+def filename() -> str:
+    return "filename"
+
+
+@pytest.fixture()
+def corr_ensemble(filename: str, corr_data: CorrelatorData) -> CorrelatorEnsemble:
+    corr_ensemble = CorrelatorEnsemble(filename)
+    corr_ensemble.correlators = corr_data
+    return corr_ensemble
 
 
 @pytest.fixture()
@@ -42,3 +71,9 @@ def test_correlator_ensemble_allows_to_set_vevs_with_correct_data(
 ) -> None:
     CorrelatorEnsemble("filename").vevs = vev_data
     # reaching this point means it didn't raise
+
+
+def test_correlator_ensemble_reports_correct_NT(
+    corr_ensemble: CorrelatorEnsemble,
+) -> None:
+    assert corr_ensemble.NT == LENGTH_TIME
