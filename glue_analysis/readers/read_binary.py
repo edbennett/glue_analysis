50d57237e5a3f2eae7329ecdb092d491a721a9d2
Julian Lenz <j.j.lenz@swansea.ac.uk>
2023-12-18 15:02:25 +0000

    Revert "Add caching"
    This reverts commit 00c7e5d58ae8c0c7e8c457cd1d45a9afb5154081.

--- glue_analysis/readers/read_binary.py
+++ glue_analysis/readers/read_binary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from functools import lru_cache
 from pathlib import Path
 from typing import Any, BinaryIO
 
@@ -33,7 +32,6 @@ class ParsingError(Exception):
     pass
 
 
-@lru_cache(maxsize=8)
 def read_correlators_binary(
     corr_filename: str,
     vev_filename: str | None = None,
